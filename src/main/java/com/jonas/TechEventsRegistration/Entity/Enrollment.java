ad00bbe8f752aa98d9b21220c2fb522e5d66158b
Jonathan Júnior <jonathan.oliveirajunior.23@gmail.com>
2026-06-19 01:15:43 -0300

    refactor:add builder to enrollment

--- src/main/java/com/jonas/TechEventsRegistration/Entity/Enrollment.java
+++ src/main/java/com/jonas/TechEventsRegistration/Entity/Enrollment.java
@@ -3,12 +3,14 @@ package com.jonas.TechEventsRegistration.Entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
