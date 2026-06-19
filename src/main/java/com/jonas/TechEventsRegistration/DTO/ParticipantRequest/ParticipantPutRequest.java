a4bc13ac86255a29989ca258d4dba0a3a36a714e
Jonathan Júnior <jonathan.oliveirajunior.23@gmail.com>
2026-06-19 01:23:40 -0300

    feat: add @NotNull and @NotBlank validations, @NoArgsConstructor and @AllArgsConstructor and @Builder for the attributes

--- src/main/java/com/jonas/TechEventsRegistration/DTO/ParticipantRequest/ParticipantPutRequest.java
+++ src/main/java/com/jonas/TechEventsRegistration/DTO/ParticipantRequest/ParticipantPutRequest.java
@@ -1,14 +1,28 @@
 package com.jonas.TechEventsRegistration.DTO.ParticipantRequest;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ParticipantPutRequest {
+
+    @NotNull
     private Long id;
+    @NotBlank
     private String participantName;
     @Email
+    @NotBlank
     private String email;
+    @NotBlank
     private String phoneNumber;
+    @NotBlank
     private String institution;
 }
