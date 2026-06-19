24713acd2b43e4958ce1855ea5347c1b17df7bcb
Jonathan Júnior <jonathan.oliveirajunior.23@gmail.com>
2026-06-19 01:19:47 -0300

    feat: add pageable endpoint findAll and restrict endpoint to ADMIN role

--- src/main/java/com/jonas/TechEventsRegistration/Controllers/EnrollmentController.java
+++ src/main/java/com/jonas/TechEventsRegistration/Controllers/EnrollmentController.java
@@ -5,8 +5,11 @@ import com.jonas.TechEventsRegistration.DTO.EnrollmentRequests.EnrollmentPutRequ
 import com.jonas.TechEventsRegistration.Entity.Enrollment;
 import com.jonas.TechEventsRegistration.Services.EnrollmentServices;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +18,12 @@ import org.springframework.web.bind.annotation.*;
 public class EnrollmentController {
 
     private final EnrollmentServices enrollmentServices;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Page<Enrollment>> findAll(Pageable pageable){
+        return new ResponseEntity<>(enrollmentServices.findAll(pageable), HttpStatus.OK);
+    }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<Enrollment> findById(@PathVariable Long id){
