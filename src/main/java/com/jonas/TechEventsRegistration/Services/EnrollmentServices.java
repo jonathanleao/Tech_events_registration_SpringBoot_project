487dec18acc1eacc8a9881dd9eecfa93c4e15f23
Jonathan Júnior <jonathan.oliveirajunior.23@gmail.com>
2026-08-18 00:06:51 -0300

    refactor : refactor all services, to return a body response, change parameter of increase vacancies for better consistence and create a new method "validateVacanciesMaxCapacity" to better control of vacancies

--- src/main/java/com/jonas/TechEventsRegistration/Services/EnrollmentServices.java
+++ src/main/java/com/jonas/TechEventsRegistration/Services/EnrollmentServices.java
@@ -1,11 +1,13 @@
 package com.jonas.TechEventsRegistration.Services;
 
-import com.jonas.TechEventsRegistration.DTO.EnrollmentRequests.EnrollmentPostRequest;
-import com.jonas.TechEventsRegistration.DTO.EnrollmentRequests.EnrollmentPutRequest;
+import com.jonas.TechEventsRegistration.DTO.Enrollment.EnrollmentRequest;
+import com.jonas.TechEventsRegistration.DTO.Enrollment.EnrollmentResponse;
 import com.jonas.TechEventsRegistration.Entity.Enrollment;
 import com.jonas.TechEventsRegistration.Entity.Event;
 import com.jonas.TechEventsRegistration.Entity.Participant;
+import com.jonas.TechEventsRegistration.Exceptions.NoVacanciesAvailableException;
 import com.jonas.TechEventsRegistration.Exceptions.NotFoundException;
+import com.jonas.TechEventsRegistration.Exceptions.VacanciesLimitExceedException;
 import com.jonas.TechEventsRegistration.Mappers.EnrollmentMapper;
 import com.jonas.TechEventsRegistration.Repository.EnrollmentRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,58 +25,70 @@ public class EnrollmentServices {
     private final EventServices eventServices;
 
 
-    public Page<Enrollment> findAll(Pageable pageable){
-        return enrollmentRepository.findAll(pageable);
+    public Page<EnrollmentResponse> findAll(Pageable pageable){
+        return enrollmentRepository.findAll(pageable).map(enrollmentMapper::toResponse);
     }
 
-    public Enrollment findById(Long id){
+    public EnrollmentResponse findById(Long id){
+        return enrollmentRepository.findById(id).map(enrollmentMapper::toResponse)
+                .orElseThrow(() -> new NotFoundException("enrollment not Found whit id: " + id));
+    }
+
+    public Enrollment findEnrollmentEntityById(Long id){
         return enrollmentRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Id not Found"));
+                .orElseThrow(()-> new NotFoundException("Enrollment not found with id: " + id));
     }
 
     @Transactional
-    public Enrollment save (EnrollmentPostRequest enrollmentPostRequest){
-        Participant participant = participantServices.findById(enrollmentPostRequest.getParticipantId());
-        Event event= eventServices.findById(enrollmentPostRequest.getEventId());
-        Enrollment enrollment = enrollmentMapper.enrollmentToPost(enrollmentPostRequest);
-        enrollment.setEvent(event);
-        enrollment.setParticipant(participant);
+    public EnrollmentResponse save (EnrollmentRequest enrollmentRequest){
+        Event event = eventServices.findEventEntityById(enrollmentRequest.getEventId());
+        Participant participant = participantServices.findParticipantEntityById(enrollmentRequest.getParticipantId());
+        Enrollment entity = enrollmentMapper.toEntity(enrollmentRequest);
+        entity.setEvent(event);
+        entity.setParticipant(participant);
         decreaseVacancies(event);
-        return enrollmentRepository.save(enrollment);
+        enrollmentRepository.save(entity);
+        return enrollmentMapper.toResponse(entity);
     }
     @Transactional
-    public Enrollment update(EnrollmentPutRequest enrollmentPutRequest){
-        Event event = eventServices.findById(enrollmentPutRequest.getEventId());
-        Participant participant = participantServices.findById(enrollmentPutRequest.getParticipantId());
-        Enrollment enrollment = findById(enrollmentPutRequest.getId());
+    public EnrollmentResponse update(Long id, EnrollmentRequest enrollmentRequest){
+        Enrollment enrollment = findEnrollmentEntityById(id);
+        Event event = eventServices.findEventEntityById(enrollmentRequest.getEventId());
+        Participant participant = participantServices.findParticipantEntityById(enrollmentRequest.getParticipantId());
 
         if (!enrollment.getEvent().getId().equals(event.getId())){
-            increaseVacancies(enrollment);
+            increaseVacancies(enrollment.getEvent());
             decreaseVacancies(event);
         }
 
-        enrollmentMapper.enrollmentToPut(enrollmentPutRequest, enrollment);
-        enrollment.setParticipant(participant);
         enrollment.setEvent(event);
-        return enrollmentRepository.save(enrollment);
+        enrollment.setParticipant(participant);
+        enrollmentRepository.save(enrollment);
+        return enrollmentMapper.toResponse(enrollment);
 
     }
     @Transactional
     public void delete(Long id){
-        Enrollment enrollment = findById(id);
-        increaseVacancies(enrollment);
+        Enrollment enrollment = findEnrollmentEntityById(id);
+        increaseVacancies(enrollment.getEvent());
         enrollmentRepository.deleteById(id);
     }
 
     private void decreaseVacancies(Event event){
         if (event.getVacancies() <= 0){
-            throw  new IllegalStateException("No vacancies dispo for this event");
+            throw  new NoVacanciesAvailableException("No vacancies dispo for this event");
         }
         event.setVacancies(event.getVacancies() - 1);
     }
-    private void increaseVacancies(Enrollment enrollment){
-        enrollment.getEvent().setVacancies(
-                enrollment.getEvent().getVacancies() + 1
-        );
+    private void increaseVacancies(Event event){
+        Integer newVacancies = event.getVacancies() + 1;
+        validateVacanciesMaxCapacity(newVacancies, event.getMaxVacancies());
+        event.setVacancies(newVacancies);
     }
+    private void validateVacanciesMaxCapacity(Integer vacancies, Integer maxVacancies){
+        if (vacancies > maxVacancies){
+            throw  new VacanciesLimitExceedException("vacancies cant not Exceed the limit");
+        }
+    }
+
 }
