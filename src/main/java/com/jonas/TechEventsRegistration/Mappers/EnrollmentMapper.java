a5f77a3200dc7959c857cca2cdc6b93837b362f0
Jonathan Júnior <jonathan.oliveirajunior.23@gmail.com>
2026-08-18 00:08:18 -0300

    refactor : fix mapper "toResponse"

--- src/main/java/com/jonas/TechEventsRegistration/Mappers/EnrollmentMapper.java
+++ src/main/java/com/jonas/TechEventsRegistration/Mappers/EnrollmentMapper.java
@@ -1,17 +1,16 @@
 package com.jonas.TechEventsRegistration.Mappers;
 
 import com.jonas.TechEventsRegistration.DTO.Enrollment.EnrollmentRequest;
-import com.jonas.TechEventsRegistration.DTO.Enrollment.EnrolmentResponse;
+import com.jonas.TechEventsRegistration.DTO.Enrollment.EnrollmentResponse;
 import com.jonas.TechEventsRegistration.Entity.Enrollment;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring", uses = {ParticipantMapper.class, EventMapper.class})
 public interface EnrollmentMapper {
 
     Enrollment toEntity (EnrollmentRequest enrollmentRequest);
 
-    EnrolmentResponse toResponse (Enrollment enrollment);
+    EnrollmentResponse toResponse (Enrollment enrollment);
 
 }
