5d0089f1095329bafca274de6b3972eaef1648a3
Jonathan Júnior <jonathan.oliveirajunior.23@gmail.com>
2026-09-07 19:54:38 -0300

    refactor : add authentication manager, and authentication provider, and changer security filter chain to use a STATELESS session, add authentication with jwt and filters

--- src/main/java/com/jonas/TechEventsRegistration/Configs/SecurityConfig.java
+++ src/main/java/com/jonas/TechEventsRegistration/Configs/SecurityConfig.java
@@ -1,56 +1,65 @@
 package com.jonas.TechEventsRegistration.Configs;
 
+import com.jonas.TechEventsRegistration.SecurityServices.CustomUserDetailsServices;
+import com.jonas.TechEventsRegistration.SecurityServices.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsServices userDetailsServices;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+        @Bean
+        public SecurityFilterChain securityFilterChain (HttpSecurity http, JwtAuthenticationFilter jwtAuthFilter) {
+            http
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .sessionManagement(session -> session
+                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                            .requestMatchers("/auth/**").permitAll()
+                            .requestMatchers("/Participants/**", "/Events/**", "/Enrollments/**").permitAll()
+                            .anyRequest().authenticated())
+                    .authenticationProvider(authenticationProvider())
+                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+            return http.build();
+        }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http){
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/Participants/**", "/Events/**", "/Enrollments/**").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-        return http.build();
+    AuthenticationProvider authenticationProvider (){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsServices);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
+
+
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User.builder()
-                .username("Jonathan Leão")
-                .password(passwordEncoder().encode("adminSenha"))
-                .roles("USER", "ADMIN")
-                .build();
-
-        UserDetails user2 = User.builder()
-                .username("Jonas")
-                .password(passwordEncoder().encode("userSenha"))
-                .roles("USER")
-                .build();
-
-        return  new InMemoryUserDetailsManager(user,user2);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config){
+        return  config.getAuthenticationManager();
     }
 }
