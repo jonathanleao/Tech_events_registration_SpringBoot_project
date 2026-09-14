3c0cb96bf8321bd25390c6edcb09eedaddc1435d
Jonathan Júnior <jonathan.oliveirajunior.23@gmail.com>
2026-09-14 15:53:44 -0300

    feat: add @NotNull to my role attribute

--- src/main/java/com/jonas/TechEventsRegistration/DTO/SecurityDTOs/UserRegisterRequest.java
+++ src/main/java/com/jonas/TechEventsRegistration/DTO/SecurityDTOs/UserRegisterRequest.java
@@ -2,6 +2,7 @@ package com.jonas.TechEventsRegistration.DTO.SecurityDTOs;
 
 import com.jonas.TechEventsRegistration.Entity.UserRoles;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,6 @@ public class UserRegisterRequest {
     private String login;
     @NotBlank
     private String password;
-    @NotBlank
+    @NotNull
     private UserRoles role;
 }
